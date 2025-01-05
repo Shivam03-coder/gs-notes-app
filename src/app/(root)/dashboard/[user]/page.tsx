210ba171bcf915cc6a2c46aeff0389fe9be65054
shivam anand <shivam850anand@gmail.com>
2025-01-05 21:20:34 +0530

    sidebar main corrected

--- src/app/(root)/dashboard/[user]/page.tsx
+++ src/app/(root)/dashboard/[user]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const User = () => {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+    <div className="flex text-pretty text-primary flex-1 flex-col gap-4 p-4 pt-0">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="bg-green-700 aspect-video rounded-xl" />
         <div className="bg-red-700 aspect-video rounded-xl" />
